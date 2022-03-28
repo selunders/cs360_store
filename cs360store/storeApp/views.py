6ed7aa6621691359a2623af5740872ba0a74fd8a
Seth Lunders <sethlunders@gmail.com>
2022-03-28 13:22:51 -0700

    Add python code for home page view.

--- cs360store/storeApp/views.py
+++ cs360store/storeApp/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import ShippingAddress, BillingAddress, ProductTag, ServiceTag, Vendor, Invoice, ProductListing, ServiceListing, InvoiceProduct, InvoiceService
+
+
+def index(request):
+    """View function for home page of the site."""
+
+    num_vendors = Vendor.objects.all().count()
+    num_sales = Invoice.objects.count()
+    num_productsActive = ProductListing.objects.filter(active=True).count()
+    num_servicesActive = ServiceListing.objects.filter(active=True).count()
+
+    context = {
+        'num_vendors': num_vendors,
+        'num_sales': num_sales,
+        'num_productsActive':  num_productsActive,
+        'num_servicesActive': num_servicesActive,
+    }
+
+    return render(request, 'index.html', context = context)
