df28eb4c3ed88b342b565caa898a0dbbae71b585
Seth Lunders <sethlunders@gmail.com>
2022-04-01 12:26:56 -0700

    Update models & admin site

--- cs360store/storeApp/admin.py
+++ cs360store/storeApp/admin.py
@@ -1,3 +1,4 @@
+from dataclasses import fields
 from django.contrib import admin
 
 # Register your models here.
@@ -12,27 +13,48 @@ class Inline_ShippingAddress(admin.TabularInline):
     extra = 1
 
 
+class Inline_InvoiceProduct(admin.StackedInline):
+    model = InvoiceProduct
+    fieldsets = [(None, {'fields': ('product', 'vendor', 'unit_price', 'quantity_ordered', 'status')})]
+    readonly_fields = ['vendor', 'unit_price']
+    # readonly_fields = ['vendor_name', 'product']
+    extra = 0
+    def get_readonly_fields(self, request, obj=None):
+        if obj: #This is the case when obj is already created i.e. it's an edit
+            return ['vendor', 'unit_price','product', 'quantity_ordered']
+        else:
+            return ['vendor', 'unit_price']    
+
+class Inline_InvoiceService(admin.StackedInline):
+    model = InvoiceService
+    fieldsets = [(None, {'fields': ('service', 'vendor', 'price_paid', 'status')})]
+    
+    # readonly_fields = ['vendor_name', 'service']
+    extra = 0
+    def get_readonly_fields(self, request, obj=None):
+        if obj: #This is the case when obj is already created i.e. it's an edit
+            return ['vendor', 'price_paid', 'service']
+        else:
+            return ['vendor',]
+
 # class Inline_ProductListing(admin.TabularInline):
 class Inline_ProductListing(admin.StackedInline):
     model = ProductListing
+    list_display = ('name', 'list_tags', 'vendor', 'price', 'quantity_available', 'active')
+    inlines = [Inline_InvoiceProduct]
     extra = 0
 
 # class Inline_ServiceListing(admin.TabularInline):
 class Inline_ServiceListing(admin.StackedInline):
     model = ServiceListing
+    readonly_fields = ['vendor',]
     extra = 0
 
-class Inline_InvoiceProduct(admin.StackedInline):
-    model = InvoiceProduct
-    extra = 0
-
-class Inline_InvoiceService(admin.StackedInline):
-    model = InvoiceService
-    extra = 0
 
 class Inline_Invoice(admin.TabularInline):
     model = Invoice
     show_change_link = True
+    readonly_fields = ['total_payment',]
     extra = 0
 
 # class Inline_ProductTag(admin.TabularInline):
@@ -62,21 +84,25 @@ class VendorAdmin(admin.ModelAdmin):
             'fields': ('address1', 'address2', 'zip_code', 'city', 'state_or_province', 'country'),
         }),
     ]
+    search_fields = ['vendor_name']
 
-    inlines = [Inline_Invoice, Inline_ProductListing, Inline_ServiceListing]
+    inlines = [Inline_ProductListing, Inline_ServiceListing]
 
 class InvoiceAdmin(admin.ModelAdmin):
-    list_display = ('vendor', 'id', 'date_placed')
-    list_filter = ('vendor', 'date_placed')
+    list_display = ('id', 'purchaser', 'date_placed', 'total_payment')
+    list_filter = ('date_placed',)
     inlines = [Inline_InvoiceProduct, Inline_InvoiceService]
+    search_fields = ['id']
 
 class ProductListingAdmin(admin.ModelAdmin):
     list_filter = ('active','vendor', )
     list_display = ('name', 'list_tags', 'vendor', 'price', 'quantity_available', 'active')
+    readonly_fields = ['vendor', ]
 
 class ServiceListingAdmin(admin.ModelAdmin):
     list_filter = ('active', 'vendor')
     list_display = ('name', 'list_tags', 'vendor', 'price', 'days_available', 'active')
+    readonly_fields = ['vendor', ]
 
 admin.site.register(Vendor, VendorAdmin)
 admin.site.register(Invoice, InvoiceAdmin)
--- cs360store/storeApp/models.py
+++ cs360store/storeApp/models.py
@@ -1,9 +1,12 @@
-# from tkinter import CASCADE
+from ast import Delete
+from email.policy import default
 from django.db import models
 from datetime import datetime
 from django.contrib.auth.models import User
 from django.urls import reverse # generate URLS by reversing URL patterns
 import uuid # for unique instances
+from django.db.models import F # for better database access
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
 
@@ -40,11 +43,23 @@ class ProductTag(models.Model):
     """Model representing a tag for a product."""
     name = models.CharField(max_length=200, help_text="Enter a tag; i.e. \"electronics\"", primary_key=True, unique=True)
     # product = models.ManyToManyField(ProductListing, blank=True)
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(ProductTag, self).save(*args, **kwargs)
 
 class ServiceTag(models.Model):
     """Model representing a tag for a service."""
     name = models.CharField(max_length=200, help_text="Enter a tag; i.e. \"electronics\"", primary_key=True, unique=True)
     # service = models.ManyToManyField(ServiceListing, blank=True)
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(ServiceTag, self).save(*args, **kwargs)
 # class BillingAddress(ShippingAddress):
     # tax_number = models.IntegerField(max_length=10, help_text="Tax Number")
 
@@ -78,13 +93,29 @@ class Invoice(models.Model):
     """Model representing an invoice for an order"""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular order", editable=False)
     purchaser = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    vendor = models.ForeignKey(Vendor, on_delete=models.RESTRICT, null=True)
     date_placed = models.DateTimeField(default=datetime.now, blank=True)
-    payment_amount = models.DecimalField(decimal_places=2, max_digits=9)
-    order_fulfilled = models.BooleanField(default=False)
-
+    # payment_amount = models.DecimalField(decimal_places=2, max_digits=9)
+    total_payment = models.DecimalField(decimal_places=2, max_digits=7, default=0)
     def __str__(self):
         return str(self.id)
+
+    def get_absolute_url(self):
+        """Returns the url to access this order's page."""
+        return reverse('my-orders-detail', args=[str(self.id)])
+    # @property
+    # def total_payment(self):
+        # """Return sum of payments for attached products/services."""
+        # return float(InvoiceProduct.objects.filter(invoice__exact=self.id).aggregate(total_payment=models.Sum(F('unit_price') * F('quantity_ordered'))).get('total_payment') or 0) \
+            # + float(InvoiceService.objects.filter(invoice__exact=self.id).aggregate(total_payment=models.Sum(F('price_paid'))).get('total_payment') or 0)
+    
+    def save(self, *args, **kwargs):
+        self.total_payment = float(InvoiceProduct.objects.filter(invoice__exact=self.id).aggregate(total_payment=models.Sum(F('unit_price') * F('quantity_ordered'))).get('total_payment') or 0) \
+            + float(InvoiceService.objects.filter(invoice__exact=self.id).aggregate(total_payment=models.Sum(F('price_paid'))).get('total_payment') or 0)
+        super(Invoice, self).save(*args, **kwargs)
+    # @property
+    # def total_payment(self):
+    #     """Return sum of payments for attached products/services."""
+    #     return self.total_products + self.total_services
 
 class ProductListing(models.Model):
     """Model representing a product for sale."""
@@ -105,7 +136,7 @@ class ProductListing(models.Model):
         self.active = state
     
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_absolute_url(self):
         """Returns the url to access details for this product."""
@@ -137,7 +168,7 @@ class ServiceListing(models.Model):
         self.active = state
     
     def __str__(self):
-        return self.name
+        return str(self.name)
 
     def get_absolute_url(self):
         """Returns the url to access details for this service."""
@@ -150,12 +181,100 @@ class ServiceListing(models.Model):
 
 class InvoiceProduct(models.Model):
     """Model representing a product in an order."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular service", editable=False)
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE,)
     product = models.ForeignKey(ProductListing, on_delete=models.RESTRICT)
-    price_paid = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.RESTRICT, blank=True)
+    unit_price = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    quantity_ordered = models.IntegerField(default=1, validators=[MaxValueValidator(9999)])
+    # total_payment = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    ORDER_STATUS = (
+        ('or', 'Order Received'),
+        ('cl', 'Canceled'),
+        ('sh', 'Shipped'),
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=ORDER_STATUS,
+        blank=True,
+        default='or',
+        help_text='Order Status',
+    )
+
+    def is_shipped(self):
+        if self.status == 'sh':
+            return True
+        return False
+
+    def is_canceled(self):
+        if self.status == 'cl':
+            return True
+        return False
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.unit_price = self.product.price
+        self.vendor = self.product.vendor
+        super(InvoiceProduct, self).save(*args, **kwargs)
+        self.invoice.save()
+
+    def delete(self, *args, **kwargs):
+        parentInvoice = self.invoice
+        super(InvoiceProduct, self).delete(*args, **kwargs)
+        parentInvoice.save()
+    
+
+    # @property
+    # def get_payment_amount(self):
+    #     return self.unit_price
+
 
 class InvoiceService(models.Model):
     """Model representing a service in an order."""
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Unique ID for this particular service", editable=False)
     invoice = models.ForeignKey(Invoice, on_delete=models.CASCADE)
     service = models.ForeignKey(ServiceListing, on_delete=models.RESTRICT)
     price_paid = models.DecimalField(decimal_places=2, max_digits=7, default=0)
+    vendor = models.ForeignKey(Vendor, on_delete=models.RESTRICT, blank=True)
+    ORDER_STATUS = (
+        ('sc', 'Scheduled'),
+        ('cl', 'Canceled'),
+        ('cm', 'Completed'),
+    )
+    status = models.CharField(
+        max_length=2,
+        choices=ORDER_STATUS,
+        blank=True,
+        default='sc',
+        help_text='Order Status',
+    )
+
+    def is_completed(self):
+        if self.status == 'cm':
+            return True
+        return False
+
+    def is_canceled(self):
+        if self.status == 'cl':
+            return True
+        return False
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.price_paid = self.service.price
+        self.vendor = self.service.vendor
+        super(InvoiceService, self).save(*args, **kwargs)
+        self.invoice.save()
+
+    def delete(self, *args, **kwargs):
+        parentInvoice = self.invoice
+        super(InvoiceService, self).delete(*args, **kwargs)
+        parentInvoice.save()
+
+    
